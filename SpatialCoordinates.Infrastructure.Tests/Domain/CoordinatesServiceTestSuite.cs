32e23cff29878a3a5a92d5212043e1fb31d76552
SergioMillaPineda <132048058+SergioMillaPineda@users.noreply.github.com>
2023-06-12 14:03:17 +0200

    Added testing project

---
+++ SpatialCoordinates.Infrastructure.Tests/Domain/CoordinatesServiceTestSuite.cs
@@ -0,0 +1,36 @@
+﻿using SpatialCoordinates.Domain.ServiceImplementations;
+using SpatialCoordinates.Infrastructure.Data.RepositoryImplementations;
+using SpatialCoordinates.WebAPI.Controllers;
+using System.Collections.Generic;
+using System.Web.Http.Results;
+using System.Web.Http;
+using Xunit;
+using SpatialCoordinates.Domain.DomainEntities;
+
+namespace SpatialCoordinates.Infrastructure.Tests.Domain
+{
+    public class CoordinatesServiceTestSuite
+    {
+        [Fact]
+        public void IntegrationTest_Register_InputValid_ReturnsVoid()
+        {
+            // Arrange
+            CoordinatesService service = new CoordinatesService(
+                new CoordinatesRepository()
+            );
+
+            Coordinates input = new Coordinates
+            {
+                CoordX = 1,
+                CoordY = 1,
+                CoordZ = 1
+            };
+
+            // Act
+            var exception = Record.Exception(() => service.Register(input));
+
+            // Assert
+            Assert.Null(exception);
+        }
+    }
+}
---
+++ SpatialCoordinates.Infrastructure.Tests/WebAPI/CoordinatesControllerTestSuite.cs
@@ -0,0 +1,61 @@
+﻿using Moq;
+using SpatialCoordinates.Domain.DomainEntities;
+using SpatialCoordinates.Domain.RepositoryContracts;
+using SpatialCoordinates.Domain.ServiceImplementations;
+using SpatialCoordinates.Infrastructure.Data.RepositoryImplementations;
+using SpatialCoordinates.WebAPI.Controllers;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Xunit;
+
+namespace SpatialCoordinates.Infrastructure.Tests.WebAPI
+{
+    public class CoordinatesControllerTestSuite
+    {
+        #region Register
+        [Fact]
+        public void IntegrationTest_Register_InputValid_ReturnsOkResult()
+        {
+            // Arrange
+            CoordinatesController controller = new CoordinatesController(
+                new CoordinatesService(
+                    new CoordinatesRepository()
+                    ));
+
+            List<decimal> values = new List<decimal> { 0, 0, 0 };
+
+            // Act
+            IHttpActionResult result = controller.Register(values);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void UnitTest_Register_InputValid_ReturnsOkResult()
+        {
+            // TODO smilla -> complete code with correct Mocking
+            // Arrange
+            Mock<CoordinatesService> serviceMock = new Mock<CoordinatesService>();
+            serviceMock.Setup(x => x.Register(new Coordinates()
+            {
+                CoordX = 0,
+                CoordY = 0,
+                CoordZ = 0
+            }));
+
+            CoordinatesService mockedService = serviceMock.Object;
+            CoordinatesController controller = new CoordinatesController(mockedService);
+
+            List<decimal> values = new List<decimal> { 0, 0, 0 };
+
+            // Act
+            IHttpActionResult result = controller.Register(values);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+        #endregion
+    }
+}
